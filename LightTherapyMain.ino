c00129c92825536ef53bf82f1d9d4489f9a6640a
Claire Chen <lairecay01@gmail.com>
2021-11-14 16:37:27 -0800

    Added logic for no sun hours

--- LightTherapyMain.ino
+++ LightTherapyMain.ino
@@ -12,7 +12,7 @@ const int PWM_Ch12 = 12;
 const int freq = 5000;
 const int res = 8;
 
-static int val = 255;
+static int LEDIndex = 0;
 
 // Network credentials
 const char* ssid     = "Stream Darkside Imp";
@@ -42,7 +42,7 @@ static int scaledCurrentTime = 0;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
-const int coolLEDVal[256] = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 
+const int coolLEDVal[257] = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 
                              32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 
                              64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 
                              96, 98, 100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124, 126, 
@@ -57,9 +57,9 @@ const int coolLEDVal[256] = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 
                              126, 124, 122, 120, 118, 116, 114, 112, 110, 108, 106, 104, 102, 100, 98, 96, 
                              94, 92, 90, 88, 86, 84, 82, 80, 78, 76, 74, 72, 70, 68, 66, 64, 
                              62, 60, 58, 56, 54, 52, 50, 48, 46, 44, 42, 40, 38, 36, 34, 32, 
-                             30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0};
+                             30, 28, 26, 24, 22, 20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 0, 0};
                              
-const int warmLEDVal[256] = {254, 252, 250, 248, 246, 244, 242, 240, 238, 236, 234, 232, 230, 228, 226, 224, 
+const int warmLEDVal[257] = {254, 252, 250, 248, 246, 244, 242, 240, 238, 236, 234, 232, 230, 228, 226, 224, 
                              222, 220, 218, 216, 214, 212, 210, 208, 206, 204, 202, 200, 198, 196, 194, 192, 
                              190, 188, 186, 184, 182, 180, 178, 176, 174, 172, 170, 168, 166, 164, 162, 160, 
                              158, 156, 154, 152, 150, 148, 146, 144, 142, 140, 138, 136, 134, 132, 130, 128, 
@@ -74,7 +74,7 @@ const int warmLEDVal[256] = {254, 252, 250, 248, 246, 244, 242, 240, 238, 236, 2
                              128, 130, 132, 134, 136, 138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158, 
                              160, 162, 164, 166, 168, 170, 172, 174, 176, 178, 180, 182, 184, 186, 188, 190, 
                              192, 194, 196, 198, 200, 202, 204, 206, 208, 210, 212, 214, 216, 218, 220, 222, 
-                             224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254};              
+                             224, 226, 228, 230, 232, 234, 236, 238, 240, 242, 244, 246, 248, 250, 252, 254, 0};              
 
 // Function to increment interrupt counter
 void IRAM_ATTR onTimer() {
@@ -97,8 +97,7 @@ void setup() {
   // Initialize timer: prescaler
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 1000, true);
-//  timerAlarmWrite(timer, 60000000, true);
+  timerAlarmWrite(timer, 60000000, true);
   timerAlarmEnable(timer);
 
   // Connect to WIFI
@@ -149,19 +148,19 @@ void ISR() {
     // getHours() (from 0 to 23) | getMinutes() (from 0 to 59)
 
     // turn current hours and minutes into a value [0:1]
-//    scaledCurrentTime = ((timeClient.getHours() * 60 + timeClient.getMinutes()) - SUNRISE) / 
-//      (SUNSET - SUNRISE);
-
+    scaledCurrentTime = ((timeClient.getHours() * 60 + timeClient.getMinutes()) - SUNRISE) / 
+      (SUNSET - SUNRISE);
+    if (scaledCurrentTime < 0 || scaledCurrentTime > 1) {
+      // before sunrise or after sunset
+      LEDIndex = 256;
+    }
+    else {
+      LEDIndex = round(scaledCurrentTime * 255);
+    }
+    
     // check if scaledCurrentTime is valid
-//    ledcWrite(PWM_Ch13, warmLEDVal[round(scaledCurrentTime * 255)]);
-//    ledcWrite(PWM_Ch12, coolLEDVal[round(scaledCurrentTime * 255)]);
-
-    val--;
-    if (val == 0) 
-      val = 255;
-      
-    ledcWrite(PWM_Ch13, warmLEDVal[val]);
-    ledcWrite(PWM_Ch12, coolLEDVal[val]);
+    ledcWrite(PWM_Ch13, warmLEDVal[LEDIndex]);
+    ledcWrite(PWM_Ch12, coolLEDVal[LEDIndex]);
 }
 
 void loop() {
